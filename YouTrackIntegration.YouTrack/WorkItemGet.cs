591f6efe3059bab72561a60c2e0361ece4d44025
VladislavAripov <vlad.aripov.007@gmail.com>
2021-06-06 14:43:37 +0300

    YouTrackIntegration.YouTrack removed.

--- YouTrackIntegration.YouTrack/WorkItemGet.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace YouTrackIntegration.YouTrack
-{
-    public class WorkItemGet
-    {
-        public string id { get; set; }
-        
-        public string text { get; set; }
-    }
-}
--- YouTrackIntegration.YouTrack/YouTrackUser.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace YouTrackIntegration.YouTrack
-{
-    public class YouTrackUser
-    {
-        public string id { get; set; }
-        
-        public string name { get; set; }
-    }
-}
